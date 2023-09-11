71a5755d1c9b78407b3f6d41a11d43853c325f72
JoaquinMaciel2910 <joaquinmaciel2910@gmail.com>
2023-09-11 15:25:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function obtenerJugadaUsuario() {
+    var jugada = prompt("Elige: piedra, papel o tijera").toLowerCase();
+    if (jugada !== "piedra" && jugada !== "papel" && jugada !== "tijera") {
+        alert("Por favor, elige una opción válida.");
+        return obtenerJugadaUsuario();
+    }
+    return jugada;
+}
+
+function obtenerJugadaComputadora() {
+    var opciones = ["piedra", "papel", "tijera"];
+    var indice = Math.floor(Math.random() * 3);
+    return opciones[indice];
+}
+
+function determinarGanador(jugadaUsuario, jugadaComputadora) {
+    switch (jugadaUsuario) {
+        case "piedra":
+            return jugadaComputadora === "tijera" ? "¡Ganaste!" : "¡Perdiste!";
+        case "papel":
+            return jugadaComputadora === "piedra" ? "¡Ganaste!" : "¡Perdiste!";
+        case "tijera":
+            return jugadaComputadora === "papel" ? "¡Ganaste!" : "¡Perdiste!";
+    }
+    return "¡Es un empate!";
+}
+
+function jugarPiedraPapelTijera() {
+    for (var i = 0; i < 3; i++) {
+        var jugadaUsuario = obtenerJugadaUsuario();
+        var jugadaComputadora = obtenerJugadaComputadora();
+        var resultado = determinarGanador(jugadaUsuario, jugadaComputadora);
+        alert("Vos elegiste: " + jugadaUsuario + "\nLa computadora eligió: " + jugadaComputadora + "\nResultado: " + resultado);
+    }
+}
+
+jugarPiedraPapelTijera();
